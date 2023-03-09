c695967038ff7f0f5e7e848ebb4e94c024b12ce0
TkNazarii <tknazkooo@gmail.com>
2023-03-09 14:24:07 +0200

    add click on card country

--- src/index.js
+++ src/index.js
@@ -9,54 +9,72 @@ const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const cartInfo = document.querySelector('.country-info');
 
+// const item = document.querySelector('.country-item');
+// const img = document.querySelector('.country-img');
+// const item = document.querySelector('.country-item');
+
 input.addEventListener('input', debounce(sendValue, DEBOUNCE_DELAY))
 
+list.addEventListener('click', choice)
+
+function choice(evt) {
+	
+	if (evt.target.className === 'country-list') {
+		return;
+	}
+	
+	const textHTML = evt.target.innerHTML
+	// console.log(textHTML);
+	input.value = textHTML
+	fetchThen(input.value)
+}
+
 function sendValue(event) {
-	event.preventDefault();
+	event.preventDefault()
 	list.innerHTML = ''
 	cartInfo.innerHTML = ''
-
-	const myValue = input.value.trim()
-	console.log(myValue);	
-
+	const myValue = input.value.trim()	
+	
 	if (!myValue) {
 		return
 	}
 
-		fetchCountries(myValue)
+	fetchThen(myValue)
+
+		}
+		
+		function createMarkup(arr) {
+			
+			const markap = arr.map(({ name, flags: { svg } }) => `<li class="country-item"><img class="country-img" src="${svg}" alt="flag" width=50px><h2 class="country-text">${name}</h2>
+				</li>`
+			)
+			
+			list.innerHTML = markap.join('')
+		}
+		
+		function createMarkupInfo(arr) {
+			const markap = arr.map(({ capital, population, languages: [{ name }] }) => `<p class="country-info">Capital: ${capital}</p><p class="country-info">Рopulation: ${population}</p><p class="country-info">Languages: ${name}</p>`)
+
+			cartInfo.innerHTML = markap.join('')
+		}
+
+
+		function fetchThen(value) {
+		
+			fetchCountries(value)
 			.then((data) => {
-				
 				if (data.length > 10) {
-					console.log('arr > 10');
 					Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
 				} else if (data.length === 1) {		
 					createMarkup(data)
 					createMarkupInfo(data)
 				} else {
-					createMarkup(data)
+							createMarkup(data)
+						}
+					})
+					.catch((error) => {
+						console.log(error);
+						Notiflix.Notify.failure("Oops, there is no country with that name");
+					})
 				}
-			})
-			.catch((error) => {
-				console.log(error);
-					Notiflix.Notify.failure("Oops, there is no country with that name");
-			})
-	
-
-}
-
-function createMarkup(arr) {
-	const markap = arr.map(({ name, flags: { svg } }) => `<li class="country-item"><img src="${svg}" alt="flag" width=50px><h2>${name}</h2>
-</li>`
-	)
-
-	list.innerHTML = markap.join('')
-}
-
-function createMarkupInfo(arr) {
-	const markap = arr.map(({ capital, population, languages: [{ name }] }) => `<p class="country-info">Capital: ${capital}</p><p class="country-info">Рopulation: ${population}</p><p class="country-info">Languages: ${name}</p>`)
-
-	cartInfo.innerHTML = markap.join('')
-}
-
-
 
